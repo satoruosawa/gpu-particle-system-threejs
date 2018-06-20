43c34995b764c88a71e1b196644a23adfbac31d2
SatoruOsawa <osawa@takram.com>
2018-06-21 05:26:06 +0900

    add renderTargetArray

--- src/js/app/ParticleMap.js
+++ src/js/app/ParticleMap.js
@@ -4,21 +4,47 @@ export default class ParticleMap {
   constructor(size) {
     this.bufScene = new Three.Scene()
     const geometry = new Three.PlaneGeometry(size, size)
+    this.uniforms = {
+      prevTexture: { type: 't', value: null },
+      textureSize: { type: 'f', value: size },
+    }
     const shaderMaterial = this.allocShader()
     const mesh = new Three.Mesh(geometry, shaderMaterial)
     this.bufScene.add(mesh)
     this.bufCamera = new Three.Camera()
-    this.renderTarget = new Three.WebGLRenderTarget(size, size)
+    this.numRenderTargets = 2
+    this.renderTargetArray = []
+    this.renderTargetArray[0] = new Three.WebGLRenderTarget(size, size)
+    this.renderTargetArray[1] = new Three.WebGLRenderTarget(size, size)
+    this.renderTargetIndex = 0
   }
 
   allocShader() {
     return new Three.ShaderMaterial({
+      uniforms: this.uniforms,
       vertexShader: document.getElementById( 'vertexShader' ).textContent,
       fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
     })
   }
 
   render(renderer) {
+    this.updateUniforms()
+    this.shiftRenderTarget()
     renderer.render(this.bufScene, this.bufCamera, this.renderTarget)
+  }
+
+  updateUniforms() {
+    this.uniforms.prevTexture.value = this.renderTarget.texture
+  }
+
+  shiftRenderTarget() {
+    this.renderTargetIndex++
+    if (this.renderTargetIndex >= this.numRenderTargets) {
+      this.renderTargetIndex = 0
+    }
+  }
+
+  get renderTarget() {
+    return this.renderTargetArray[this.renderTargetIndex]
   }
 }
