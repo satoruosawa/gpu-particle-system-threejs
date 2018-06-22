90fbd09cf111135053529d7d45cdfffc33d40980
SatoruOsawa <osawa@takram.com>
2018-06-22 14:01:30 +0900

    add allocateDataTexture function

--- src/js/app/ParticleModel.js
+++ src/js/app/ParticleModel.js
@@ -5,24 +5,35 @@ import fragmentShader from '../../shader/particle-model-fragment.glsl'
 
 export default class ParticleModel {
   constructor (size) {
+    this.size = size
     this.bufScene = new Three.Scene()
     const geometry = new Three.PlaneGeometry(size, size)
     this.uniforms = {
       prevTexture: { type: 't', value: null },
-      textureSize: { type: 'f', value: size }
+      textureSize: { type: 'i', value: size }
     }
-    const shaderMaterial = this.allocShader()
+    const shaderMaterial = this.allocateShader()
     const mesh = new Three.Mesh(geometry, shaderMaterial)
     this.bufScene.add(mesh)
     this.bufCamera = new Three.Camera()
     this.numRenderTargets = 2
     this.renderTargetArray = []
-    this.renderTargetArray[0] = new Three.WebGLRenderTarget(size, size)
-    this.renderTargetArray[1] = new Three.WebGLRenderTarget(size, size)
+    this.renderTargetArray[0] = this.allocateRenderTarget(size)
+    this.renderTargetArray[1] = this.allocateRenderTarget(size)
     this.renderTargetIndex = 0
+
+    this.uniforms.prevTexture.value = this.allocateDataTexture()
   }
 
-  allocShader () {
+  allocateRenderTarget (size) {
+    return new Three.WebGLRenderTarget(size, size,
+      {
+        minFilter: Three.NearestFilter,
+        magFilter: Three.NearestFilter,
+      })
+  }
+
+  allocateShader () {
     return new Three.ShaderMaterial({
       uniforms: this.uniforms,
       vertexShader,
@@ -30,10 +41,26 @@ export default class ParticleModel {
     })
   }
 
+  allocateDataTexture () {
+    const data = new Uint8Array(this.size * this.size * 4)
+    for (let i = 0; i < this.size * this.size; i++) {
+      const index = i * 4
+      data[index] = 0  // r
+      data[index + 1] = 0  // g
+      data[index + 2] = 0  // b
+      data[index + 3] = 0  // a
+    }
+    data[0] = 255
+    const texture = new Three.DataTexture(data, this.size,
+      this.size, Three.RGBAFormat, Three.UnsignedByteType)
+    texture.needsUpdate = true
+    return texture
+  }
+
   render (renderer) {
-    this.updateUniforms()
     this.shiftRenderTarget()
     renderer.render(this.bufScene, this.bufCamera, this.renderTarget)
+    this.updateUniforms()
   }
 
   updateUniforms () {
--- src/shader/particle-model-fragment.glsl
+++ src/shader/particle-model-fragment.glsl
@@ -1,9 +1,22 @@
 uniform sampler2D prevTexture;
-uniform float textureSize;
+uniform int textureSize;
+
+int getTextureIndex() {
+  return int(gl_FragCoord.x) + int(gl_FragCoord.y) * textureSize;
+}
+
+vec2 getCoordFromIndex(int index) {
+  return vec2(mod(float(index), float(textureSize)), index / textureSize);
+}
 
 void main() {
-  vec4 prevColor = texture2D(prevTexture,
-    (floor(gl_FragCoord.xy) + 0.5) / textureSize);
-  prevColor.rgb = prevColor.rgb + 0.01;
+  int index = getTextureIndex() - 1;
+  if (index < 0) {
+    index = index + textureSize * textureSize;
+  }
+  vec4 prevColor = texture2D(
+    prevTexture,
+    getCoordFromIndex(index) / float(textureSize)
+  );
   gl_FragColor = vec4(prevColor.rgb, 1.0);
 }
